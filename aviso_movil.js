4d21d2e8d61b4b7a835806211896b494f47517a6
pacosanzz <147283350+pacosanzz@users.noreply.github.com>
2023-11-12 16:13:17 +0100

    Create aviso_movil.js

---
+++ aviso_movil.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        alert("Esta web no está optimizada para dispositivos móviles.");
+
+        setTimeout(function() {
+          window.close();
+        }, 3000);
+      }
+    };
